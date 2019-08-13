6e406a538d4a7102a7b1868de3818491ac02ebfa
Leana <leananeparidze.com>
2019-08-13 16:53:18 -0700

    adds moment js library and displays when task was completed

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import { Icon } from 'semantic-ui-react';
+import moment from 'moment';
 
 function Todo({ todo, toggleCompleted }) {
   const [completed] = useState('completed');
 
   return (
-    <div className={todo.completed ? completed : null}
-         onClick={() => toggleCompleted(todo.id)}
+    <div onClick={() => toggleCompleted(todo.id)}
          style={{fontSize: '18px', padding: '10px'}}
     >
       <Icon name="check" style={{color: '#9bf59b', opacity: `${todo.completed ? '1' : '0'}`}} />
-      {todo.item}
+      <span className={todo.completed ? completed : null}>{todo.item}</span>
+      <div style={{paddingLeft: '10px', fontSize: '10px', marginTop: '5px', height: '10px'}}>{todo.completed ? `Completed: ${moment().format('MMMM Do YYYY, h:mm:ss a')}` : null}</div>
     </div>
   )
 }
